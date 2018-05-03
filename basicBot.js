24d5b93a9ffa0470ff160d0b8f0b19bd0bd14e43
Pandorianim <34489349+Pandorianim@users.noreply.github.com>
2018-05-03 19:37:02 +0200

    Poprawka adresu plików językowych

--- basicBot.js
+++ basicBot.js
@@ -112,7 +112,7 @@
 
     var loadChat = function(cb) {
         if (!cb) cb = function() {};
-        $.get('https://rawgit.com/basicBot/source/master/lang/langIndex.json', function(json) {
+        $.get('https://rawgit.com/oliwier975PL/source/master/lang/langIndex.json', function(json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== 'undefined') {
                 langIndex = json;
