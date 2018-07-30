d6dba6c64b7300040c1ec0a5a424210a843b2057
oliwier975PL <33328258+oliwier975PL@users.noreply.github.com>
2018-07-31 00:26:30 +0200

    Update basicBotDoTestowLol.js

--- basicBotDoTestowLol.js
+++ basicBotDoTestowLol.js
@@ -911,9 +911,6 @@
             if (botCreatorIDs.indexOf(user.id) > -1) {
               console.log(true);
                 API.sendChat('@'+user.username+' '+'witaj Creator-sama, twa wola jest moim rozkazem :bow:');
-            } else if (Korapt.indexOf(user.id) > -1) {
-              console.log(true);
-                API.sendChat('@'+user.username+' '+'Korapt sie znowu zepsul :(');
             } else if (OliwierDwa.indexOf(user.id) > -1) {
               console.log(true);
                 API.sendChat('@'+user.username+' '+'Hello darkness, my old friend');
